3324f085bd5bd86517aaf17b1f506ec5a3e536e1
Gabriel Alencar <151469608+gabriel-teix@users.noreply.github.com>
2024-02-19 11:07:14 -0300

    Add files via upload

---
+++ components/Button.js
@@ -0,0 +1,22 @@
+import { View, Text, StyleSheet } from 'react-native'
+
+export function Button() {
+  return(
+    <View>
+      <Text style={styles.container}>Reserve sua Viagem</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#B0C4DE',
+    color: '#0000FF',
+    fontWeight: 'bold',
+    fontSize: 25,
+    textAlign: 'center',
+    padding: 20,
+    margin: 15,
+    marginTop: 35,
+  }  
+})
